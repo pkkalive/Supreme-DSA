dd1c3a829a574b7c8c0740254f32fdf8f32e1a78
pkkalive <kalivepurushothamkumar@gmail.com>
2022-11-14 14:06:21 +0530

    reverseArray

--- arrays/reverseArray.js
+++ arrays/reverseArray.js
@@ -2,9 +2,9 @@
     Given an array reverse in-place
  */
 function reverseArray (arr){
-    let start = 0, end = arr.length
-    while (start < end){
-        [arr[start], arr[end-1]] = [arr[end-1], arr[start]]
+    let start = 0, end = arr.length -1
+    while (start <= end){
+        [arr[start], arr[end]] = [arr[end], arr[start]]
         start++
         end--
     }
