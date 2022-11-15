e115722a140b62bf971cb071f2f3ee882c9b049f
pkkalive <kalivepurushothamkumar@gmail.com>
2022-11-15 15:26:24 +0530

    secondLargest

--- arrays/secondLargest.js
+++ arrays/secondLargest.js
@@ -1,5 +1,6 @@
 /*
-    You are given an integer array A. You have to find the second largest element/value in the array or report that no such element exists.
+    You are given an integer array A. You have to find the second largest element/value in the array or report
+    that no such element exists.
  */
 
 // Using sorting O(NlogN)
