b20f1526a52837bf1ec1bceed1bb50660f027bce
pkkalive <kalivepurushothamkumar@gmail.com>
2022-11-15 15:26:13 +0530

    searchElement

--- arrays/searchElement.js
+++ arrays/searchElement.js
@@ -1,5 +1,6 @@
 /*
-    You are given an integer T (number of test cases). You are given array A and an integer B for each test case. You have to tell whether B is present in array A or not.
+    You are given an integer T (number of test cases). You are given array A and an integer B for each test
+    case. You have to tell whether B is present in array A or not.
  */
 
 function searchElement(nums, key){
