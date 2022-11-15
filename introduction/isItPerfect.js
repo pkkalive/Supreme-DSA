6a8de5b605eb596cef73f0b795b235c9a35ee9bd
pkkalive <kalivepurushothamkumar@gmail.com>
2022-11-15 15:23:24 +0530

    isItPerfect

--- introduction/isItPerfect.js
+++ introduction/isItPerfect.js
@@ -1,7 +1,8 @@
 /*
     You are given N positive integers.
     For each given integer A, you have to tell whether it is a perfect number or not.
-    A perfect number is a positive integer which is equal to the sum of its proper positive divisors (excluding the number itself).
+    A perfect number is a positive integer which is equal to the sum of its proper positive divisors (excluding
+    the number itself).
     A positive proper divisor divides a number without leaving any remainder.
  */
 
