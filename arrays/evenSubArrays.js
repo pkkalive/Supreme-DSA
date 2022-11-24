f2833202c0c3c5b157e2761549f7f73ba753fef8
pkkalive <kalivepurushothamkumar@gmail.com>
2022-11-24 23:12:27 +0530

    evenSubArrays

--- arrays/evenSubArrays.js
+++ arrays/evenSubArrays.js
@@ -6,5 +6,12 @@
  */
 
 function evenSubArrays(nums){
-
+    const n = nums.length
+    if (nums[0] % 2 === 0 && nums[n - 1] % 2 === 0 && n % 2 === 0){
+        return "YES"
+    }
+    return "NO"
 }
+
+console.log(evenSubArrays([2, 4, 8, 6]))
+console.log(evenSubArrays([2, 4, 8, 7, 6]))
